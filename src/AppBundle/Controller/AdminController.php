4f884641711cfde3d34b79d8dc535543fb494dbe
Mikhail Syniuk <msyniuk@gmail.com>
2017-12-14 04:09:17 +0300

    admin controller edit

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -4,11 +4,14 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\User;
+use AppBundle\Entity\Report;
+use AppBundle\Form\ReportType;
 use AppBundle\Service\Reports;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class AdminController extends Controller
@@ -62,10 +65,64 @@ class AdminController extends Controller
         $months = $reports->getMonths($year);
 
         $reports = $reports->getReports($user, $from, $to);
+        $editForm = $this->createForm(ReportType::class, new Report());
+        $addForm = $this->createForm(ReportType::class, new Report());
 
         // replace this example code with whatever you need
         return $this->render('admin/report.html.twig', [
             'reports' => $reports, 'months' => $months, 'from' => $from, 'user' => $user,
+            'year' => $year, 'month' => $month,
+            'editForm' => $editForm->createView(), 'addForm' => $addForm->createView(),
         ]);
     }
+
+    /**
+     * @Route("/admin/report/save/{year}/{month}/{userid}/{id}", name="admin_report_save")
+     */
+    public function saveReport(
+        Request $request, EntityManagerInterface $em, Reports $reports,
+        $year, $month, $userid, $id = null)
+    {
+        if ( $id === null ) {
+            $report = new Report();
+            $report->setDate(new \DateTime($year . '-' . $month . '-01'));
+            $report->setUser($this->getUser());
+            $em->persist($report);
+        } else {
+            $report = $em->find(Report::class, $id);
+
+            if ( !$report ) {
+                throw new NotFoundHttpException();
+            }
+        }
+
+        $form = $this->createForm(ReportType::class, $report);
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted()) {
+            return new Response('Form not submitted.');
+        }
+
+        $result = ['success' => true];
+
+        if ($form->isValid()) {
+            $em->flush();
+
+            $from = new \DateTime(date($year . '-' . $month . '-01'));
+            $to = clone $from;
+            $to->add(new \DateInterval('P1M'));
+            $reports = $reports->getReports($this->getUser(), $from, $to);
+
+            $result['data'] = $this->renderView(
+                'admin/report_table.html.twig',
+                ['reports' => $reports]
+            );
+        } else {
+            $result['success'] = false;
+            $result['errors'] = $form->getErrors(true);
+        }
+
+        return new JsonResponse($result);
+    }
+
 }
